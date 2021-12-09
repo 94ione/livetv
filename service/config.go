05f0bbd7e73076bf99e218f30db59b2cff88e7dc
94ione <95640892+94ione@users.noreply.github.com>
2021-12-09 15:52:04 +0800

    Update config.go

--- service/config.go
+++ service/config.go
@@ -2,8 +2,8 @@ package service
 
 import (
 	"github.com/jinzhu/gorm"
-	"github.com/zjyl1994/livetv/global"
-	"github.com/zjyl1994/livetv/model"
+	"github.com/94ione/livetv/global"
+	"github.com/94ione/livetv/model"
 )
 
 func GetConfig(key string) (string, error) {
